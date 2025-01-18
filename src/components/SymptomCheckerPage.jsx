7eec8e537febde167f09f4afdbd83bb51ceca624
Abdelrahman Noaman <novaxaboud@gmail.com>
2025-01-18 12:24:11 +0200

    adjustments

--- src/components/SymptomCheckerPage.jsx
+++ src/components/SymptomCheckerPage.jsx
@@ -139,12 +139,19 @@ function SymptomCheckerPage() {
       className="font-cool flex flex-col"
       style={{ height: "calc(100vh - 75px)" }}
     >
-      
+      <div className="flex items-center ml-2 py-4">
+        <div className="h-10 w-10 bg-gray-200 rounded-full ring-1 ring-gray-300 flex items-center justify-center">
+          <TbMedicalCrossCircle size={24} className="text-blue-500" />
+        </div>
+        <h1 className="ml-4 text-[20px] md:text-2xl font-bold text-gray-800">
+          Symptom Checker
+        </h1>
+      </div>
 
       {/* Chat Section */}
       <div
         ref={chatRef}
-        className="flex-1 overflow-y-auto p-4 space-y-4 mt-2 ring-gray-300 ring-1 rounded-3xl bg-white"
+        className="flex-1 overflow-y-auto px-4 py-4 space-y-4 ring-gray-300 ring-1 rounded-3xl bg-white"
       >
         {messages.map((message) => (
           <div
@@ -155,7 +162,7 @@ function SymptomCheckerPage() {
           >
             <div className="flex items-start">
               {message.sender === "system" && (
-                <div className="">
+                <div className="mr-1">
                   <TbMedicalCrossCircle size={24} className="text-blue-500 mt-[6px]" />
                 </div>
               )}
