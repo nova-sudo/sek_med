2baba71d1b0a7d90c258e7f12c2f0ff330622cf7
Abdelrahman Noaman <novaxaboud@gmail.com>
2025-02-02 03:48:22 +0200

    landing page changes UI

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,10 +2,16 @@ import React, { useEffect, useState } from 'react';
 import Spline from '@splinetool/react-spline';
 import { Link } from 'react-router-dom';
 import sekmed_logo from '../sekmed_big.png';
+import Noise from '../Animations/Noise/Noise'
+import SplitText from "../TextAnimations/SplitText/SplitText";
+
+
 function LandingPage() {
   const [deferredPrompt, setDeferredPrompt] = useState(null); // State to store the install prompt event
   const [isInstallable, setIsInstallable] = useState(false); // State to track if app is installable
-
+  const handleAnimationComplete = () => {
+    console.log('All letters have animated!');
+  };
   useEffect(() => {
     const handleBeforeInstallPrompt = (e) => {
       e.preventDefault(); // Prevent automatic prompt
@@ -33,7 +39,8 @@ function LandingPage() {
   };
 
   return (
-    <div className="h-screen relative flex items-center justify-center">
+    <div className="h-screen relative flex items-center justify-center overflow-y-hidden">
+      
 <img 
   src={sekmed_logo} 
   alt="Sekmed Logo" 
@@ -41,39 +48,53 @@ function LandingPage() {
 />
 
       {/* Centered h1 */}
-      <h1 className="absolute sm:text-9xl md:text-header md:mb-72 md:mt-0 text-9xl font-aqem font-extrabold mb-80 mt-12 translate-y-20 z-[60] text-center">
-        SEK_
-      </h1>
-      <h1 className="absolute sm:text-9xl md:text-header md:mt-44 text-9xl mb-16 font-aqem font-extrabold mt-10 translate-y-20 text-center z-[60] ">
-        *MED
-      </h1>
-      
-      {/* Clickable Link */}
-      <Link to="/home">
-        <button className="font-cool  md:mt-[500px] md:text-4xl bg-black text-white rounded-full hover:ring-2 hover:ring-black hover:bg-white hover:text-black animate-pulse  translate-y-28 text-2xl mb-44 px-4 py-2 mt-96 ">
-          Get Started
-        </button>
-      </Link>
+      <SplitText
+  text="SEKMED"
+  className="absolute md:ml-36 ml-6 drop-shadow-md sm:text-9xl md:text-[350px] md:mt-10 text-7xl mb-16 font-cool text-white font-extrabold mt-10  text-center z-[60] "
+  delay={150}
+  animationFrom={{ opacity: 0, transform: 'translate3d(0,50px,0)' }}
+  animationTo={{ opacity: 1, transform: 'translate3d(0,0,0)' }}
+  easing="easeOutCubic"
+  threshold={0.2}
+  rootMargin="-50px"
+  onLetterAnimationComplete={handleAnimationComplete}
+/>
+     
 
       {/* Install Button */}
 {isInstallable && (
   <button
     onClick={handleInstallClick}
-    className="fixed top-5 right-5 z-[60] bg-black rounded-full md:text-2xl text-lg text-white px-4 py-2 font-cool shadow-lg hover:bg-white hover:ring-2 hover:ring-black hover:text-black"
+    className="fixed top-5 right-5 z-[60] bg-White drop-shadow-md rounded-full md:text-2xl text-lg text-Black px-4 py-2 font-cool shadow-lg hover:bg-white hover:ring-0 bg-white hover:drop-shadow-xl hover:ring-black hover:text-black"
   >
     Install App
   </button>
 )}
+ 
 
 
       {/* Background iframe */}
       <Spline
-        className="absolute z-50 pointer-events-none" // Prevent Spline from capturing clicks
+        className="absolute pointer-events-none" // Prevent Spline from capturing clicks
         scene="https://prod.spline.design/cP9EAK5hihk29Fn7/scene.splinecode"
         width="100%"
         height="100%"
       />
+      <Noise
+    patternSize={250}
+    patternScaleX={2}
+    patternScaleY={2}
+    patternRefreshInterval={100}
+    patternAlpha={15}
+  />
+  {/* Clickable Link */}
+ <Link to="/home">
+        <button className="font-cool z-[100] drop-shadow-md  md:mt-[550px] md:text-4xl bg-white   rounded-full  hover:drop-shadow-2xl hover:ring-0 hover:bg-white hover:text-black animate-pulse   text-2xl mb-44 px-4 py-2 mt-72 ">
+          Get Started
+        </button>
+      </Link>
     </div>
+    
   );
 }
 
